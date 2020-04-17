5528af85827c0ba448cc01b89c766be0b2a43bd4
Necks5 <mr.sab.artyr@gmail.com>
2020-04-17 17:36:03 +0300

    5hw

--- src/network/mt_nonblocking/Connection.h
+++ src/network/mt_nonblocking/Connection.h
@@ -2,8 +2,21 @@
 #define AFINA_NETWORK_MT_NONBLOCKING_CONNECTION_H
 
 #include <cstring>
+#include <atomic>
+#include <memory>
+#include <stdexcept>
+
+
+#include <afina/execute/Command.h>
+#include <afina/network/Server.h>
+#include "protocol/Parser.h"
+#include <afina/logging/Service.h>
 
 #include <sys/epoll.h>
+
+namespace spdlog {
+    class logger;
+}
 
 namespace Afina {
 namespace Network {
@@ -11,12 +24,15 @@ namespace MTnonblock {
 
 class Connection {
 public:
-    Connection(int s) : _socket(s) {
+    Connection(int s, std::shared_ptr<Afina::Storage> ps, std::shared_ptr<spdlog::logger> pl) :
+                    _socket(s), pStorage(ps), _logger(pl) {
         std::memset(&_event, 0, sizeof(struct epoll_event));
         _event.data.ptr = this;
+        cur_pos = 0;
+        now_pos = 0;
     }
 
-    inline bool isAlive() const { return true; }
+    inline bool isAlive() const { return _is_alive.load(); }
 
     void Start();
 
@@ -32,6 +48,15 @@ private:
 
     int _socket;
     struct epoll_event _event;
+    std::shared_ptr<spdlog::logger> _logger;
+    std::shared_ptr<Afina::Storage> pStorage;
+
+    std::atomic<bool> _is_alive;
+    std::unique_ptr<Execute::Command> command_to_execute;
+    char client_buffer[4096];
+    std::vector<std::string> buffer;
+    int cur_pos;
+    int now_pos;
 };
 
 } // namespace MTnonblock
